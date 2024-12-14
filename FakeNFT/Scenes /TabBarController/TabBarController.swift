d6f794aa73ba501d059da3317a947e5b9be0b7c6
abondarenko <aleksei.bondarenko@epassi.com>
2024-12-14 14:39:46 +0200

    Logger was added

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -12,6 +12,11 @@ final class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Example of using Logger
+        Logger.log("It’s just a test and should be removed in the future. Level is Info by default")
+        Logger.log("It's just test and should be removed in the future", level: .debug)
+        Logger.log("It's just test and should be removed in the future", level: .error)
 
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
