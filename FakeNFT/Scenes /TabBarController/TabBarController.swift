e07ec263af4118067c68458fe1986f6cb15aac0a
abondarenko <aleksei.bondarenko@epassi.com>
2024-12-15 11:20:46 +0200

    Удалены строки по использованию логгера

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,30 +1,25 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
+    
     var servicesAssembly: ServicesAssembly!
-
+    
     private let catalogTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.catalog", comment: ""),
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 0
     )
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Example of using Logger
-        Logger.log("It’s just a test and should be removed in the future. Level is Info by default")
-        Logger.log("It's just test and should be removed in the future", level: .debug)
-        Logger.log("It's just test and should be removed in the future", level: .error)
-
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         catalogController.tabBarItem = catalogTabBarItem
-
+        
         viewControllers = [catalogController]
-
+        
         view.backgroundColor = .systemBackground
     }
 }
