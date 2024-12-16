ad8024b1035ec7678c2aba23f9e914fb782e4fea
Simon <mocki.toki@gmail.com>
2024-12-16 17:32:51 +0300

    Рефакторинг RatingView

--- FakeNFT/Scenes/Common/Views/RatingView.swift
+++ FakeNFT/Scenes/Common/Views/RatingView.swift
@@ -10,9 +10,11 @@ import Then
 import UIKit
 
 class RatingView: UIView {
-    // MARK: Constants
+    // MARK: - Constants
     enum Constants {
         static let maximumRating = 5
+        static let starSize = CGSize(width: 12, height: 12)
+        static let starSpacing: CGFloat = 2
     }
 
     // MARK: - Properties
@@ -22,7 +24,7 @@ class RatingView: UIView {
         }
     }
 
-    private var starViews: [UIImageView] = []
+    private var starStackView: UIStackView!
 
     // MARK: - Initialization
     override init(frame: CGRect) {
@@ -37,35 +39,36 @@ class RatingView: UIView {
 
     // MARK: - Setup UI
     private func setupUI() {
+        starStackView = UIStackView().then {
+            $0.axis = .horizontal
+            $0.alignment = .center
+            $0.distribution = .equalSpacing
+            $0.spacing = Constants.starSpacing
+        }
+        addSubview(starStackView)
+
+        starStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
         for index in 1...Constants.maximumRating {
             let starView = UIImageView().then {
                 $0.tag = index
                 $0.image = .inactiveStar
             }
-            starViews.append(starView)
-            addSubview(starView)
+            starStackView.addArrangedSubview(starView)
 
             starView.snp.makeConstraints { make in
-                make.size.equalTo(CGSize(width: 12, height: 12))
-                if index == 1 {
-                    make.leading.equalToSuperview()
-                } else {
-                    make.leading.equalTo(starViews[index - 2].snp.trailing).offset(2)
-                }
-                if index == Constants.maximumRating {
-                    make.trailing.equalToSuperview()
-                }
+                make.size.equalTo(Constants.starSize)
             }
         }
     }
 
     // MARK: - Update UI
     private func updateStars() {
-        for starView in starViews {
-            if starView.tag <= currentRating {
-                starView.image = .activeStar
-            } else {
-                starView.image = .inactiveStar
+        for starView in starStackView.arrangedSubviews {
+            if let imageView = starView as? UIImageView {
+                imageView.image = imageView.tag <= currentRating ? .activeStar : .inactiveStar
             }
         }
     }
