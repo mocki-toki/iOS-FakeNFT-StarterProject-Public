086018df4a740bcdb58b48f2f0e6a9456dabf979
Simon <mocki.toki@gmail.com>
2024-12-17 17:22:25 +0300

    Добавлен CartButton

---
+++ FakeNFT/Scenes/Common/Views/CartButton.swift
@@ -0,0 +1,42 @@
+import SnapKit
+import UIKit
+
+final class CartButton: UIButton {
+    // MARK: - Properties
+    private var isInCart: Bool = false {
+        didSet {
+            updateAppearance()
+        }
+    }
+
+    // MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        updateAppearance()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+        updateAppearance()
+    }
+
+    // MARK: - Public methods
+
+    func setInCart(_ isInCart: Bool) {
+        self.isInCart = isInCart
+    }
+
+    // MARK: - Private methods
+
+    private func setupView() {
+        imageView?.contentMode = .scaleAspectFit
+        setImage(.addToCart, for: .normal)
+    }
+
+    private func updateAppearance() {
+        setImage(isInCart ? .removeFromCart : .addToCart, for: .normal)
+    }
+}
