859d10acb9aed77fd7f272b0ed1be3c585b0d412
calvinkalin <ilya.muromec888@gmail.com>
2024-12-19 23:26:23 +0300

    Подготовил проект для верстки кодом и настроил UINavigationController

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -2,14 +2,22 @@ import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    
     let servicesAssembly = ServicesAssembly(
         networkClient: DefaultNetworkClient(),
         nftStorage: NftStorageImpl()
     )
-
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        let tabBarController = window?.rootViewController as? TabBarController
-        tabBarController?.servicesAssembly = servicesAssembly
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let tabBarController = TabBarController(servicesAssembly: servicesAssembly)
+        tabBarController.servicesAssembly = servicesAssembly
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = tabBarController
+        window.makeKeyAndVisible()
+        
+        self.window = window
     }
 }
