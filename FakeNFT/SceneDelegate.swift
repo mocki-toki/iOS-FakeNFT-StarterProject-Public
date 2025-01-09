540c27d77153f02197340f1f9dddea8bc98e9bee
abondarenko <aleksei.bondarenko@epassi.com>
2025-01-09 21:10:26 +0200

    Splash screen has been implemented

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -5,7 +5,6 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    private let viewModel = SceneViewModel()
     private let servicesAssembly = ServicesAssembly(
         networkClient: DefaultNetworkClient(),
         nftStorage: NftStorageImpl()
@@ -16,35 +15,13 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions) {
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            window = UIWindow(windowScene: windowScene)
-            
-            if viewModel.isFirstLaunch {
-                Logger.log("App launched for the first time, showing onboarding")
-                showOnboarding()
-            } else {
-                Logger.log("App launched, showing TabBar")
-                showTabBar()
-            }
-            
-            window?.makeKeyAndVisible()
-        }
-    
-    // MARK: - Private Methods
-    
-    private func showOnboarding() {
-        let onboardingViewController = OnboardingPageViewController()
-        let navigationController = UINavigationController(rootViewController: onboardingViewController)
-        onboardingViewController.onboardingCompleted = { [weak self] in
-            self?.viewModel.setHasSeenOnboarding()
-            self?.showTabBar()
-        }
-        window?.rootViewController = navigationController
-    }
-    
-    private func showTabBar() {
-        let tabBarController = TabBarController(servicesAssembly: servicesAssembly)
-        window?.rootViewController = tabBarController
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let splashViewController = SplashViewController(servicesAssembly: servicesAssembly)
+        window?.rootViewController = splashViewController
+        window?.makeKeyAndVisible()
     }
 }
